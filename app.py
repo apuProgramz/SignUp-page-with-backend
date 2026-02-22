e230960cefac06452809afb200e828746d84b69a
Apurva Ghare <apughare6@gmail.com>
2026-02-23 00:25:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
+import json
+from pymongo import MongoClient
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/' # Secret key for flashing messages
+
+# MongoDB Atlas connection
+uri = "mongodb+srv://dummy:1234@apurvadb.gl0f3.mongodb.net/?appName=ApurvaDB"
+client = MongoClient(uri)
+db = client['ApurvaDB']
+collection = db['users']
+
+@app.route('/', methods=['GET']) 
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('username')
+    password = request.form.get('Password')
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+@app.route('/view', methods=['GET'])
+def view():
+    try:
+        users = list(collection.find({}, {"_id": 0}))  # Exclude the _id field
+        return jsonify(users)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
