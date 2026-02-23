16f734bef899f27e3f3a13ed4ab14a4df20b1d84
Apurva Ghare <apughare6@gmail.com>
2026-02-23 20:27:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,5 +33,4 @@ def view():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
     app.run(debug=True, host='127.0.0.1', port=5500)
